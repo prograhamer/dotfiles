24e50cd087534ebb690cb3eb1520c0a731546de1
Graham Brereton <graham.brereton@thescore.com>
2016-05-27 09:53:06 -0400

    Initial commit

---
+++ .bash_personal.sh
@@ -0,0 +1,36 @@
+CYAN="\[$(tput setaf 6)\]"
+GREEN="\[$(tput setaf 2)\]"
+RED="\[$(tput setaf 1)\]"
+CRESET="\[$(tput sgr0)\]"
+
+function set_prompt() {
+  export PS1="$(date +%H%M) $CYAN\w$CRESET"
+
+  if git rev-parse HEAD &>/dev/null; then
+    GITC="$GREEN"
+    STAR=""
+
+    if ! git diff-files --quiet; then
+      GITC="$RED"
+    elif ! git diff-index --quiet --cached HEAD; then
+      STAR="*"
+    fi
+
+    export PS1="$PS1:$GITC$(git rev-parse --abbrev-ref HEAD)$STAR$CRESET"
+  fi
+
+  export PS1="$PS1> "
+}
+
+export PROMPT_COMMAND="set_prompt"
+
+export CLICOLOR=1
+export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
+
+export EDITOR='vim'
+
+alias ll='ls -l'
+alias la='ls -Al'
+alias view='vim -MR'
+alias js='python -m json.tool'
+alias grep='grep --color=auto --exclude ".*.swp" --exclude ".*.swo" --exclude "ruby.tags" --exclude "python.tags"'
---
+++ .vimrc
@@ -0,0 +1,29 @@
+set expandtab
+set smarttab
+set bs=2
+set ts=2
+set sw=2
+filetype plugin indent on
+syntax on
+set ruler
+colo elflord
+execute pathogen#infect()
+set tags+=ruby.tags,python.tags
+set completeopt=menu,preview
+set hlsearch
+
+autocmd FileType python set ts=4
+autocmd FileType python set sw=4
+
+vnoremap # :s/^\([^#]\)/#\1/g <bar> :noh
+vnoremap ! :s/^#//g <bar> :noh
+
+nnoremap <leader>d :!git diff HEAD %
+nnoremap <leader>h :noh
+
+autocmd FileType ruby nnoremap <leader>r :!rubocop %
+autocmd FileType ruby nnoremap <leader>l :execute "!rspec " . expand("%") . ":" . line(".")
+autocmd FileType ruby nnoremap <leader>t :!rspec %
+autocmd FileType python nnoremap <leader>t :!py.test %
+
+set pastetoggle=<leader>p
