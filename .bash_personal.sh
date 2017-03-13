6649a4f90d20b6503fd5dd5c0e62b0b363ff5606
Graham Brereton <graham.brereton@thescore.com>
2017-03-13 12:49:49 -0400

    Multiple additions from the past months

--- .bash_personal.sh
+++ .bash_personal.sh
@@ -3,9 +3,14 @@ GREEN="\[$(tput setaf 2)\]"
 RED="\[$(tput setaf 1)\]"
 CRESET="\[$(tput sgr0)\]"
 
-function set_prompt() {
-  export PS1="$(date +%H%M) $CYAN\w$CRESET"
+function exit_status() {
+  status=$?
+  if [[ $status -ne 0 ]]; then
+    echo "$RED\\\$?=$status$CRESET "
+  fi
+}
 
+function git_status() {
   if git rev-parse HEAD &>/dev/null; then
     GITC="$GREEN"
     STAR=""
@@ -16,10 +21,12 @@ function set_prompt() {
       STAR="*"
     fi
 
-    export PS1="$PS1:$GITC$(git rev-parse --abbrev-ref HEAD)$STAR$CRESET"
+    echo ":$GITC$(git rev-parse --abbrev-ref HEAD)$STAR$CRESET"
   fi
+}
 
-  export PS1="$PS1> "
+function set_prompt() {
+  export PS1="$(exit_status)$(date +%H%M) $CYAN\w$CRESET$(git_status)> "
 }
 
 export PROMPT_COMMAND="set_prompt"
@@ -29,12 +36,22 @@ export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
 
 export EDITOR='vim'
 
+export PYTHONDONTWRITEBYTECODE=1
+
 alias ll='ls -l'
 alias la='ls -Al'
 alias view='vim -MR'
 alias js='python -m json.tool'
 alias grep='grep --color=auto --exclude ".*.swp" --exclude ".*.swo" --exclude "ruby.tags" --exclude "python.tags"'
 alias ans='pushd ~/Code/ansible-playbooks'
+alias be='bundle exec'
+alias espresso='caffeinate -disu -w 1'
+alias melatonin='/System/Library/CoreServices/"Menu Extras"/User.menu/Contents/Resources/CGSession -suspend'
+alias restore_order='killall Dock'
+gr() { grep -r "$1" .; }
+gir() { grep -ir "$1" .; }
+grl() { grep -rl "$1" .; }
+girl() { grep -irl "$1" .; }
 
 function aws_cpr() {
   if [ -z "$1" ] || [ -z "$2" ]; then
